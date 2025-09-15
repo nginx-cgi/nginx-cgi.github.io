2a5259c0000507e8c85d2f34c8f50059806c36e2
Chizhong Jin <pjincz@gmail.com>
2025-09-16 04:40:56 +0800

    add version number to page

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 if [ ! -f "./node_modules/.bin/marked" ]; then
     npm install marked
 fi
@@ -12,4 +14,8 @@ cat index.html | sed -n '1,/-- doc_start --/{p}' > tmp/index.html
 ./node_modules/.bin/marked tmp/README.md >> tmp/index.html
 cat index.html | sed -n '/-- doc_end --/,${p}' >> tmp/index.html
 
-cp tmp/index.html index.html
+cat tmp/index.html | sed -n '1,/-- ver_start --/{p}' > tmp/index.html.2
+curl -s https://api.github.com/repos/pjincz/nginx-cgi/releases/latest | jq -r .tag_name >> tmp/index.html.2
+cat tmp/index.html | sed -n '/-- ver_end --/,${p}' >> tmp/index.html.2
+
+cp tmp/index.html.2 index.html
